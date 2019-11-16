7b486c802a35d30bbd0f4c5a7a724fbe90f8b84b
Dahlmannnnn <54683052+Dahlmannnnn@users.noreply.github.com>
2019-11-15 23:38:09 -0700

    11.15a

---
+++ Coding_Project_2.m
@@ -0,0 +1,20 @@
+
+% Alexander Dahlmann
+% EE599 - Coding Project 2
+%
+function Coding_Project_2(~)
+datRaw = fopen('data.txt','r');
+datRaw
+% for j=2:1:167 
+%     for i=7:1:167
+%         if datRaw(j,i)=='-'
+%             sign = -1;
+%         else
+%             sign = 1;
+%         end
+% end
+
+yr = 1850:1:2015;
+
+
+end
