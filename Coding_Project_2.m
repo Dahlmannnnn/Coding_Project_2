33f0a1ce659f8eaf85b0867d754a797368aed11d
Dahlmannnnn <54683052+Dahlmannnnn@users.noreply.github.com>
2019-11-16 00:57:41 -0700

    11.16a

--- Coding_Project_2.m
+++ Coding_Project_2.m
@@ -2,19 +2,14 @@
 % Alexander Dahlmann
 % EE599 - Coding Project 2
 %
-function Coding_Project_2(~)
-datRaw = fopen('data.txt','r');
-datRaw
-% for j=2:1:167 
-%     for i=7:1:167
-%         if datRaw(j,i)=='-'
-%             sign = -1;
-%         else
-%             sign = 1;
-%         end
-% end
 
-yr = 1850:1:2015;
+a = readtable('data.txt');
+dat = (table2array((a(:,:))));
+hold on
 
+x_ax=dat(1,1):1:dat(end,1);
+y_vec = dat(:,14).';
 
+for x=1:1:166
+scatter(x_ax(x),y_vec(x),25,'r','filled')
 end
